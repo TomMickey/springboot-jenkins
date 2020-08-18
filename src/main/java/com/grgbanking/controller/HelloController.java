78b678c5ff8d7e865c091fb67aad05463320e2c5
TomMickey <18620243154@163.com>
2020-08-18 16:46:14 +0800

    添加获取配置文件名称

--- src/main/java/com/grgbanking/controller/HelloController.java
+++ src/main/java/com/grgbanking/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.grgbanking.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private WebApplicationContext webApplicationConnect;
 
     //日志输出
     private static Logger logger = LoggerFactory.getLogger(HelloController.class);
@@ -20,11 +25,10 @@ public class HelloController {
 
     @RequestMapping("/world")
     public String helloWorld(HttpServletRequest request){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        String[] profiles = context.getEnvironment().getActiveProfiles();
-        logger.info("输出路径:---> {}", request.getPathInfo());
+        logger.info("输出sessionId:---> {}", request.getSession().getId());
         logger.info("输出url:---> {}", request.getRequestURL());
-        logger.info("输出配置文件名称:----> {}", profiles);
+        String[] activeProfiles = webApplicationConnect.getEnvironment().getActiveProfiles();
+        logger.info("输出配置文件名称:----> {}", activeProfiles);
         return "Hello World!";
     }
 
